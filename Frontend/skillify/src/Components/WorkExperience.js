6f1508fc7da18028767f3ad4ad1ef073a01ffcea
Siddartha pogula <pogulaSiddharth96@gmail.com>
2024-03-24 00:49:36 +0530

    added folio detaills

--- Frontend/skillify/src/Components/WorkExperience.js
+++ Frontend/skillify/src/Components/WorkExperience.js
@@ -3,14 +3,13 @@ import React from 'react';
 const WorkExperience = ({ data }) => {
     const {jobExperience} = data?.data;
     console.log(jobExperience);
-  return (
-{/* <div name='work' className=' px-4 py-8  md:h-screen text-gray-300 bg-[#0a192f]'>
+return (
+  <div name='work' className='px-4  md:h-screen text-gray-300 bg-[#0a192f]'>
     <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
-              Work Experience </p>     
-              
-    {jobExperience.map((job, index) => (
-        <div key={index} className=" rounded-lg shadow-2xl p-6 mb-8">
+      <span className='text-4xl font-bold inline'>
+        <span className='border-b-4 border-pink-600 my-20'>Work Experience</span> </span>
+      {jobExperience.map((job, index) => (
+        <div key={index} className="rounded-lg shadow-2xl p-6 my-[3rem] mb-8">
           <h2 className="text-2xl font-bold text-gray-300 mb-2">{job.companyName}</h2>
           <div className="flex items-center justify-between text-gray-400 mb-2">
             <p>{job.jobRole}</p>
@@ -27,14 +26,10 @@ const WorkExperience = ({ data }) => {
           </div>
         </div>
       ))}
-        
     </div>
-</div> */}
+  </div>
+); 
 
-
-
-
-  );
 };
 
 export default WorkExperience;
